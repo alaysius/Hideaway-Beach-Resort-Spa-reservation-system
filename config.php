f25cb0a1d2628688e0a80970c51ec2d2659895b9
alaysius <82561108+alaysius@users.noreply.github.com>
2021-04-14 21:58:11 -0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+// mysql connection
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'users';
+
+$db = new mysqli($hostname, $username, $password, $database);
+if($db->connect_errno)
+    die('Error ' . $this->db->connect_error);
+
+include_once('UserClass.php');
+$con = new UserClass($db);
+?>
+
