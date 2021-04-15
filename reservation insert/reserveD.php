941379cc193dc5edf768f0eca4e97e90df758798
alaysius <82561108+alaysius@users.noreply.github.com>
2021-04-15 13:28:42 +0800

    Add files via upload

---
+++ reservation insert/reserveD.php
@@ -0,0 +1,32 @@
+<?php
+
+$connection = mysqli_connect("localhost","root","");
+$db = mysqli_select_db($connection,"users");
+
+if(isset($_POST['insertdata']))
+{
+	$name = $_POST['name'];
+	$phone = $_POST['phone'];
+	$email = $_POST['email'];
+	$numberchild = $_POST['numberchild'];
+	$numberadult = $_POST['numberadult'];
+	$date = $_POST['date'];
+	$month = $_POST['month'];
+	$year = $_POST['year'];
+	$message=$_POST['message'];
+	
+	
+	$query = "INSERT INTO reserve(name, phone, email, numberchild, numberadult, date, month, year, message)VALUES ('$name','$phone','$email', '$numberchild', '$numberadult', '$date', '$month', '$year', '$message')";
+	$result = mysqli_query($connection,$query);
+	
+	if($result)
+	{
+	  echo '<script> alert("Data Saved"); </script>';
+	    header('location:room-details-D.php');
+	}
+	else
+	{
+	echo '<script> alert("Data Not Saved"); </script>';
+	}
+}
+?>
